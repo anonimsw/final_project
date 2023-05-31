28b648baacb4e5811cd844894ea9cf993caa07e1
anonimsw <118049735+anonimsw@users.noreply.github.com>
2023-05-31 17:05:17 +0500

    Add files via upload

--- models/todo.js
+++ models/todo.js
@@ -16,6 +16,9 @@ const todoSchema = new mongoose.Schema(
             type: String,
             required: true,
             trim: true,
+        },
+        phone_number: {
+            type: Number,
         }
     },
 
